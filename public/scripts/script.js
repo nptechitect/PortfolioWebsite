2f052bccc526c09c411d6230305546b6bdba47b2
Nick Parrish <NickParrish@creativeliquidcoatings.com>
2025-04-20 22:01:57 -0400

    Homepage complete for desktop/laptop screens

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,11 @@
-alert("Welcome");
+document.querySelectorAll("section.projectCard").forEach((element) => {
+  console.log(element);
+  element.addEventListener("mouseenter", (event) => {
+    if (event.target.tagName != "SECTION") { return; }
+    event.target.style.boxShadow = "2px 2px 5px black";
+  });
+  element.addEventListener("mouseleave", (event) => {
+    if (event.target.tagName != "SECTION") { return; }
+    event.target.style.boxShadow = "none";
+  })
+});

794471e60e7dfc38c429e017a544e10f5ae9b8f0
Nick Parrish <NickParrish@creativeliquidcoatings.com>
2025-04-22 09:29:45 -0400

    Swapped skills list to icons

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,6 @@
+
+// Add drop shadow when over the project card
 document.querySelectorAll("section.projectCard").forEach((element) => {
-  console.log(element);
   element.addEventListener("mouseenter", (event) => {
     if (event.target.tagName != "SECTION") { return; }
     event.target.style.boxShadow = "2px 2px 5px black";
@@ -7,5 +8,39 @@ document.querySelectorAll("section.projectCard").forEach((element) => {
   element.addEventListener("mouseleave", (event) => {
     if (event.target.tagName != "SECTION") { return; }
     event.target.style.boxShadow = "none";
+  })
+});
+
+
+// Make skill icons pop up when pointer is over them.
+document.querySelectorAll(".skillIcon").forEach((element) => {
+  element.addEventListener("mouseenter", (event) => {
+    if (event.target.tagName != "DIV") { return; }
+    event.target.style.maxWidth = "75px";
+  });
+  element.addEventListener("mouseleave", (event) => {
+    if (event.target.tagName != "DIV") { return; }
+    event.target.style.maxWidth = "50px";
+  })
+})
+
+document.querySelectorAll("#message").forEach((element) => {
+  element.addEventListener("selectionchange", (event) => {
+    const length = event.target.value.length;
+    const max = event.target.attributes.maxlength.value;
+    const charCount = event.target.nextElementSibling;
+
+    // Update the character count/max string
+    charCount.innerHTML = length + "/" + max;
+
+    // Change color to warn user is almost out of characters
+    if (length >= 200 && length < 250) {
+      charCount.style.color = "orange";
+    } else if (length >= 250) {
+      charCount.style.color = "red";
+    } else {
+      charCount.style.color = "black";
+    }
+
   })
 });

b42270bce16ca6933f2c679177dfe2ae969d821d
nptechitect <nptechitect@users.noreply.github.com>
2025-04-23 23:40:45 +0000

    deploy: a6f7e25fa58b28228f4a9896dff43794fe17e979

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,54 @@
-alert("Welcome");
+
+// Add drop shadow when over the project card
+document.querySelectorAll("section.projectCard").forEach((element) => {
+  element.addEventListener("mouseenter", (event) => {
+    if (event.target.tagName != "SECTION") { return; }
+    event.target.style.boxShadow = "2px 2px 5px black";
+  });
+  element.addEventListener("mouseleave", (event) => {
+    if (event.target.tagName != "SECTION") { return; }
+    event.target.style.boxShadow = "none";
+  })
+});
+
+
+// Make skill icons pop up when pointer is over them.
+document.querySelectorAll(".skillIcon").forEach((element) => {
+  element.addEventListener("mouseenter", (event) => {
+    if (event.target.tagName != "DIV") { return; }
+    event.target.style.maxWidth = "75px";
+  });
+  element.addEventListener("mouseleave", (event) => {
+    if (event.target.tagName != "DIV") { return; }
+    event.target.style.maxWidth = "50px";
+  })
+})
+
+document.querySelectorAll("#message").forEach((element) => {
+  element.addEventListener("selectionchange", (event) => {
+    const length = event.target.value.length;
+    const max = event.target.attributes.maxlength.value;
+    const charCount = event.target.nextElementSibling;
+
+    // Update the character count/max string
+    charCount.innerHTML = length + "/" + max;
+
+    // Change color to warn user is almost out of characters
+    if (length >= 200 && length < 250) {
+      charCount.style.color = "orange";
+    } else if (length >= 250) {
+      charCount.style.color = "red";
+    } else {
+      charCount.style.color = "black";
+    }
+
+  })
+});
+
+document.getElementById("menu_icon").addEventListener("click", (event) => {
+  const menu = document.getElementById("menuDropDown");
+
+  const current = menu.style.display || window.getComputedStyle(menu).display;
+
+  menu.style.display = (current === 'none') ? 'block' : 'none';
+});
